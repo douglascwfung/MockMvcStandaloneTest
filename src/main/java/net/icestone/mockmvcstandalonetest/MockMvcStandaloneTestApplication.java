92e3eeaf46213237c15c3b87bd755794bf0bc04e
Douglas Fung <icestone.net@gmail.com>
2022-08-05 09:37:49 +0800

    initial commit

---
+++ src/main/java/net/icestone/mockmvcstandalonetest/MockMvcStandaloneTestApplication.java
@@ -0,0 +1,13 @@
+package net.icestone.mockmvcstandalonetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockMvcStandaloneTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockMvcStandaloneTestApplication.class, args);
+	}
+
+}
